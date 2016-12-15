018497b5dae9fbc212fe428ef8d5821328c73aec
Bogon <zhangqixcu@gmail.com>
2016-12-15 17:27:52 +0800

    添加数据模型

---
+++ DLOnlineParserXML/model/DLNoteUnfoldInfomation.h
@@ -0,0 +1,13 @@
+//
+//  DLNoteUnfoldInfomation.h
+//  DLOnlineParserXML
+//
+//  Created by Char on 2016/12/15.
+//  Copyright © 2016年 zhangqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DLNoteUnfoldInfomation : NSObject
+
+@end
---
+++ DLOnlineParserXML/model/DLNoteUnfoldInfomation.m
@@ -0,0 +1,13 @@
+//
+//  DLNoteUnfoldInfomation.m
+//  DLOnlineParserXML
+//
+//  Created by Char on 2016/12/15.
+//  Copyright © 2016年 zhangqi. All rights reserved.
+//
+
+#import "DLNoteUnfoldInfomation.h"
+
+@implementation DLNoteUnfoldInfomation
+
+@end
